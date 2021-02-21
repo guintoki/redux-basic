bfd8591a1de15cdcf819f0add846627cd6ea991a
guintoki <guilherme-vilas-boas@hotmail.com>
2021-02-21 18:23:26 -0300

    init redux with store

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Catalog: React.FC = () => {
+  const store = useSelector((state) => state);
+
+  console.log(store);
+
+  return <h1>Catalog</h1>;
+};
+
+export default Catalog;
---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+
+const store = createStore(() => "Guidão");
+
+export default store;
