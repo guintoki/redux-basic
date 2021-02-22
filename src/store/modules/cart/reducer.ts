1fd0888a82db4a1dd05aeab4fe17718a4f8e8c22
guintoki <guilherme-vilas-boas@hotmail.com>
2021-02-21 21:57:04 -0300

    fix increment items

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -12,7 +12,15 @@ const cart: Reducer<ICartState> = (state = INITAL_STATE, action) => {
       case "ADD_PRODUCT_TO_CART": {
         const { product } = action.payload;
 
-        draft.items.push({ product, quantity: 1 });
+        const productInCartIndex = draft.items.findIndex(
+          (item) => item.product.id === product.id
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({ product, quantity: 1 });
+        }
 
         break;
       }
