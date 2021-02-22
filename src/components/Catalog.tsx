f21fe967686ec21d26b3855a042c24e511f7fb0b
guintoki <guilherme-vilas-boas@hotmail.com>
2021-02-21 21:13:52 -0300

    add click to add product

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import api from "../services/api";
+import { addProductToCard } from "../store/modules/cart/actions";
 import { IProduct } from "../store/modules/cart/types";
 
 const Catalog: React.FC = () => {
+  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -11,14 +14,20 @@ const Catalog: React.FC = () => {
     });
   }, []);
 
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCard(product));
+  }, []);
+
   return (
     <main>
       <h1>Catalog</h1>
       {catalog.map((product) => (
         <article key={product.id}>
           <strong>{product.title}</strong> {" - "}
-          <span>{product.price}</span> {" - "}
-          <button type="button">Comprar</button>
+          <span>{product.price}</span> {"  "}
+          <button onClick={() => handleAddProductToCart(product)} type="button">
+            Comprar
+          </button>
         </article>
       ))}
     </main>
