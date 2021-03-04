ac1e453023ec7dd862c7e1827bdcdf13b4f424b3
guintoki <guilherme-vilas-boas@hotmail.com>
2021-03-03 21:42:18 -0300

    create AcionTypes

--- src/store/modules/cart/types.ts
+++ src/store/modules/cart/types.ts
@@ -1,4 +1,8 @@
-export enum
+export enum ActionTypes {
+  addProductToCartRequest = "ADD_PRODUCT_TO_CART_REQUEST",
+  addProductToCartSuccess = "ADD_PRODUCT_TO_CART_SUCCESS",
+  addProductToCartFailure = "ADD_PRODUCT_TO_CART_FAILURE",
+}
 
 export interface IProduct {
   id: number;
